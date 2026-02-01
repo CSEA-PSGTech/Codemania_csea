f62d3d57cfc1d91e1f932a0cd0521009292dc8d4
Nimbus3001 <haroonsurkhi10@gmail.com>
2026-02-01 19:34:02 +0530

    Add TeamLogin component for player login

---
+++ mainfrontend/frontend/src/TeamLogin.jsx
@@ -0,0 +1,12 @@
+function TeamLogin() {
+  return (
+    <div className="login-card">
+      <h2>CODEMANIA PLAYER LOGIN</h2>
+      <input type="text" placeholder="Team Name" />
+      <input type="password" placeholder="Access Code" />
+      <button>ENTER ROUND 1</button>
+    </div>
+  );
+}
+
+export default TeamLogin;
